09be49d9dc9931be5faa054aa649a0c20872601d
김현진 <kimhhhhjin@gmail.com>
2025-12-04 14:44:41 +0900

    변경

---
+++ 학습/프로그램_적성검사/변수와_상수/변수와상수_실습.js
@@ -0,0 +1,18 @@
+
+
+// 이건 주석
+// 주석은 설명을 달아 놓는것으로 - 프로그램 번역이 안된다.
+// 주석은 한줄 주석이 있고 여러줄 주석이 있어. 한줄:// 여러줄은 /*    ... */
+
+
+// 줄바꿈은 newline -> \n
+console.log("\n");
+
+let userName = "홍길동";
+console.log("처음 변수 선언한 이름:" , userName);
+console.log("");  // 한줄 떨어 트리기
+// +로 하니까 한칸 떨어진게 없네
+console.log("처음 변수 선언한 이름:" + userName);
+
+console.log(true && true);
+
