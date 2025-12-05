21b23b7258c61db1bec93b7a2c1971033203130c
김현진 <kimhhhhjin@gmail.com>
2025-12-05 10:26:33 +0900

    변경

--- 학습/프로그램_적성검사/변수와_상수/변수와상수_실습.js
+++ 학습/프로그램_적성검사/변수와_상수/변수와상수_실습.js
@@ -4,16 +4,31 @@
 // 주석은 설명을 달아 놓는것으로 - 프로그램 번역이 안된다.
 // 주석은 한줄 주석이 있고 여러줄 주석이 있어. 한줄:// 여러줄은 /*    ... */
 
+ let 섭씨 = 9;
+ console.log((섭씨 * 9/5) + 32);
+ console.log("48.2 = (섭씨 * 9/5) + 32");
 
-let root = 5;
-console.log("******구구단" ,root,"단 출력******");
-console.log("-------------------------------------")
-console.log(root, "* 1 = ", root * 1);
-console.log(root, "* 2 = ", root * 2);
-console.log(root, "* 3 = ", root * 3);
-console.log(root, "* 4 = ", root * 4);
-console.log(root, "* 5 = ", root * 5);
-console.log(root, "* 6 = ", root * 6);
-console.log(root, "* 7 = ", root * 7);
-console.log(root, "* 8 = ", root * 8);
-console.log(root, "* 9 = ", root * 9);
+ let width = 5;
+ let height = 6;
+ console.log(넓이 = width * height);
+ console.log("30 = width * height");
+
+ 
+ let num1 = 12;
+ let num2 = 6;
+ let num3 = 2;
+ let result;
+
+ result = num1 + num2 + num3
+ console.log("평균:", num1, "+", num2, "+", num3, "=", result);
+
+ let 전체초 = 125;
+ console.log(분 = 전체초 / 60);
+ console.log(초 = 전체초 % 60);
+ console.log("분 = 전체초 / 60");
+ console.log("초 = 전체초 % 60");
+
+ let price = 110;
+ let count = 70;
+ console.log(총합 = price * count);
+ console.log("총합 = price x count");
