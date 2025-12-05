e61b0a3f3d5d9f05202ccfae282072a30900955b
김현진 <kimhhhhjin@gmail.com>
2025-12-05 13:01:07 +0900

    변경

--- 학습/프로그램_적성검사/변수와_상수/변수와상수_실습.js
+++ 학습/프로그램_적성검사/변수와_상수/변수와상수_실습.js
@@ -4,74 +4,69 @@
 // 주석은 설명을 달아 놓는것으로 - 프로그램 번역이 안된다.
 // 주석은 한줄 주석이 있고 여러줄 주석이 있어. 한줄:// 여러줄은 /*    ... */
 
- let 섭씨 = 9;
- console.log((섭씨 * 9/5) + 32);
- console.log("48.2 = (섭씨 * 9/5) + 32");
+ //let 섭씨 = 9;
+ //console.log((섭씨 * 9/5) + 32);
+ //console.log("48.2 = (섭씨 * 9/5) + 32");
 
-<<<<<<< HEAD
- let width = 5;
- let height = 6;
- console.log(넓이 = width * height);
- console.log("30 = width * height");
+
+ //let width = 5;
+ //let height = 6;
+ //console.log(넓이 = width * height);
+ //console.log("30 = width * height");
 
  
- let num1 = 12;
- let num2 = 6;
- let num3 = 2;
- let result;
+ //let num1 = 12;
+ //let num2 = 6;
+ //let result;
 
- result = num1 + num2 + num3
- console.log("평균:", num1, "+", num2, "+", num3, "=", result);
 
- let 전체초 = 125;
- console.log(분 = 전체초 / 60);
- console.log(초 = 전체초 % 60);
- console.log("분 = 전체초 / 60");
- console.log("초 = 전체초 % 60");
+ //result = (num1 + num2);
+ //console.log(num1 + num2 / 2); 
+ //console.log("평균:(", num1, "+", num2, ")/2 =", result);
 
- let price = 110;
- let count = 70;
- console.log(총합 = price * count);
- console.log("총합 = price x count");
-=======
-<<<<<<< HEAD
-// 줄바꿈은 newline -> \n
-console.log("\n");
+ //let price = 110;
+ //let count = 70;
+ //console.log(총합 = price * count);
+ //console.log("총합 = price x count");
 
-let userName = "홍길동";
-console.log("문자열 상수 -> 초기 이름:", userName, userName);
+//let 전체초 = 125;
+// console.log(분 = 전체초 / 60);
+// console.log(초 = 전체초 % 60);
+// console.log("분 = 전체초 / 60");
+// console.log("초 = 전체초 % 60");
 
-// 구구단 2단출력
-// 변수는 문자를 사용해서 <-- 다른값을 넣는거야
-let gugudan = 1;
-console.log("--- 구구단 " + gugudan, '단 출력 ---' )
-console.log('gugudan' , ' * 1 = ', gugudan * 1 );
-console.log(gugudan , ' * 2 = ', gugudan * 2 );
-console.log(gugudan , ' * 3 = ', gugudan * 3 );
-console.log(gugudan , ' * 4 = ', gugudan * 4 );
-console.log(gugudan , ' * 5 = ', gugudan * 5 );
-console.log(gugudan , ' * 6 = ', gugudan * 6 );
-console.log(gugudan , ' * 7 = ', gugudan * 7 );
-console.log(gugudan , ' * 8 = ', gugudan * 8 );
-console.log(gugudan , ' * 9 = ', gugudan * 9 );
-console.log('-----------------');
+//  let으로 변수 선언.
+//let username = "홍길동";
+//console.log("초기 이름:", username, username);
 
-// for(let ii=1; ii <=19; ii++ ) {
-//     console.log(dan , ' * ' , ii , ' = ', dan * ii );
-// }
+//username = "김철수";
+//console.log("변경된 이름:", username);
 
-=======
-let root = 5;
-console.log("******구구단" ,root,"단 출력******");
-console.log("-------------------------------------")
-console.log(root, "* 1 = ", root * 1);
-console.log(root, "* 2 = ", root * 2);
-console.log(root, "* 3 = ", root * 3);
-console.log(root, "* 4 = ", root * 4);
-console.log(root, "* 5 = ", root * 5);
-console.log(root, "* 6 = ", root * 6);
-console.log(root, "* 7 = ", root * 7);
-console.log(root, "* 8 = ", root * 8);
-console.log(root, "* 9 = ", root * 9);
->>>>>>> 584c39dbad022bb8d1483e2ee6e17efac8726b76
->>>>>>> 7b86cd53f3fbf4efba3143b5663be712834a33b9
+//let age = 25;
+//console.log("나이:", age);
+//age = 26;
+//console.log("변경된 나이:", age); 
+
+//const pi = 3.14159;
+//console.log("원주율:", pi);  
+
+//const compant_name = "테크컴퍼니";
+//console.log("회사명:", compant_name);
+
+//let greeting = "안녕하세요";
+//console.log("문자열:", greeting, "- 타입:", typeof greeting);
+
+//let score = 100;
+//console.log("숫자:", score, "- 타입:", typeof score);
+
+let age = 25;
+console.log("나이:", age);
+
+let height = 187.7;
+console.log("키:", height);
+
+let temperature = -10;
+console.log("온도:", temperature);
+
+let num1 = 10
+let num2 = 4 
